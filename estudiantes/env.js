d2cfa11bfb80a0246056229ab6768d044b3a115c
jerex3 <jmanuel@sinergiasoluciones.com.ar>
2024-11-13 18:36:57 -0300

    Agregue variables de ambiente para uso de mocks, y url a microservicio de matriculados

--- estudiantes/env.js
+++ estudiantes/env.js
@@ -5,6 +5,8 @@ const DB_HOST = "localhost"
 const DB_USER = "postgres"
 const DB_PASSWORD = "root"
 const DB_NAME = "turni"
+const ENROLLMENTS_BASE_URL = "http://localhost:3003/";
+const USE_MOCKS = true;
 
 module.exports = {
     PORT,
@@ -12,5 +14,7 @@ module.exports = {
     DB_HOST,
     DB_USER,
     DB_PASSWORD,
-    DB_NAME
+    DB_NAME,
+    ENROLLMENTS_BASE_URL,
+    USE_MOCKS
 }
